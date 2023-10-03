839fb1780967593271424d676fe61429749e85be
christmex <christmex@yahoo.com>
2023-10-03 14:46:43 +0700

    Patch

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        'Croustibat\FilamentJobsMonitor\Models\QueueMonitor' => 'App\Policies\QueueMonitorPolicy'
     ];
 
     /**
