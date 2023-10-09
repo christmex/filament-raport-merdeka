81b3846bf0059eb655f5200bda26d6dfec6bc45e
christmex <christmex@yahoo.com>
2023-10-09 13:53:03 +0700

    Patch

--- resources/js/move_to_another_text_field.js
+++ resources/js/move_to_another_text_field.js
@@ -1,46 +1,24 @@
 document.addEventListener("keydown", function(event) {
-    if (event.key === "Tab") {
+    if (event.key === "Tab" || event.key === "Enter" || event.key === "ArrowDown" || event.key === "ArrowUp") {
         event.preventDefault();
         const activeElement = document.activeElement;
         const numberInputs = Array.from(document.querySelectorAll("input[type=number]"));
         const currentIndex = numberInputs.indexOf(activeElement);
 
-        if (event.shiftKey) {
-            const previousIndex = currentIndex === 0 ? numberInputs.length - 1 : currentIndex - 1;
-            numberInputs[previousIndex].focus(function() { $(this).select(); });
-        } else {
+        if (event.key === "Tab" || event.key === "Enter" || event.key === "ArrowDown") {
             const nextIndex = currentIndex === numberInputs.length - 1 ? 0 : currentIndex + 1;
-            numberInputs[nextIndex].focus(function() { $(this).select(); });
-        }
-    }
-
-    if (event.key === "Enter") {
-        event.preventDefault();
-        const activeElement = document.activeElement;
-        const numberInputs = Array.from(document.querySelectorAll("input[type=number]"));
-        const currentIndex = numberInputs.indexOf(activeElement);
-
-        if (event.shiftKey) {
-            const previousIndex = currentIndex === 0 ? numberInputs.length - 1 : currentIndex - 1;
-            numberInputs[previousIndex].focus(function() { $(this).select(); });
-        } else {
-            const nextIndex = currentIndex === numberInputs.length - 1 ? 0 : currentIndex + 1;
-            numberInputs[nextIndex].focus(function() { $(this).select(); });
-        }
-    }
-
-    if (event.key === "ArrowDown" || event.key === "ArrowUp") {
-        event.preventDefault();
-        const activeElement = document.activeElement;
-        const numberInputs = Array.from(document.querySelectorAll("input[type=number]"));
-        const currentIndex = numberInputs.indexOf(activeElement);
-
-        if (event.key === "ArrowDown") {
-            const nextIndex = currentIndex === numberInputs.length - 1 ? 0 : currentIndex + 1;
-            numberInputs[nextIndex].focus(function() { $(this).select(); });
+            numberInputs[nextIndex].select();
         } else {
             const previousIndex = currentIndex === 0 ? numberInputs.length - 1 : currentIndex - 1;
-            numberInputs[previousIndex].focus(function() { $(this).select(); });
+            numberInputs[previousIndex].select();
         }
     }
+});
+
+// Add a click event listener to select all text when the input is clicked.
+const numberInputs = document.querySelectorAll("input[type=number]");
+numberInputs.forEach(input => {
+    input.addEventListener("click", function() {
+        this.select();
+    });
 });
