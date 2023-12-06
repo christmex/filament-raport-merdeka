b6fa23ef0a59db3a55dce22268ede438b9326625
christmex <christmex@yahoo.com>
2023-12-06 22:23:26 +0700

    dd

--- resources/views/exports/report-sheet.blade.php
+++ resources/views/exports/report-sheet.blade.php
@@ -60,6 +60,6 @@
 				</td>
 			</tr>
 		@endforeach
-		{{dd($finalNewData,$avgDiv, $PASDiv, Helper::countFinalGrade(85,79,$avgDiv, $PASDiv))}}
+		{{dd($finalNewData,$avgDiv, $PASDiv, Helper::countFinalGrade(85,79,$avgDiv, $PASDiv), 85*75/100+79*25/100)}}
 	</tbody>
 </table>
