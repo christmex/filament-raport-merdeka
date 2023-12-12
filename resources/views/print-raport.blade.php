dd5307f8f0aa3c5b9f99e38c5a7a4c670dbfaab5
christmex <christmex@yahoo.com>
2023-12-12 10:13:35 +0700

    Patch

--- resources/views/print-raport.blade.php
+++ resources/views/print-raport.blade.php
@@ -468,7 +468,7 @@
 	</section>
 	<div style="clear: both;"></div>
 	<section id="student_photo" style="float:right; margin-right: 100px;margin-top: 30px">
-		<div style="display: inline-block;border:1px solid black;width: 120px;height: 150px;text-align:center; line-height: 100px;margin-right: 10px">
+		<div style="display: inline-block;border:1px solid black;width: 118px;height: 150px;text-align:center; line-height: 100px;margin-right: 10px">
 			Pas foto
 			3 x4
 		</div>
