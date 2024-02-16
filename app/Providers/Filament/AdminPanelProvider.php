cd5596abd512e0e8f5fcfc9edb8753416b26da60
christmex <christmex@yahoo.com>
2024-02-16 13:56:08 +0700

    Set default theme add custom color and many more

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,7 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
+use Filament\Enums\ThemeMode;
 use App\Filament\Pages\Backups;
 use Filament\Navigation\MenuItem;
 use Filament\Support\Colors\Color;
@@ -35,8 +36,17 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('')
             ->login()
+            ->defaultThemeMode(ThemeMode::Dark)
             ->colors([
-                'primary' => Color::Amber,
+                // 'danger' => Color::Rose,
+                'danger' => '#e63946',
+                'gray' => Color::Gray,
+                // 'info' => Color::Blue,
+                'info' => '#457b9d',
+                'primary' => '#E65C00',
+                'success' => Color::Emerald,
+                // 'warning' => Color::Orange,
+                'warning' => '#ffb703',
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
@@ -77,7 +87,8 @@ class AdminPanelProvider extends PanelProvider
             ->profile(EditProfile::class)
             // ->topNavigation()
             ->sidebarCollapsibleOnDesktop()
-            ->spa()
+            // ->spa()
+            ->unsavedChangesAlerts()
             ->plugins([
                 FilamentJobsMonitorPlugin::make()
                 ->label('Job')
@@ -92,7 +103,49 @@ class AdminPanelProvider extends PanelProvider
                 FilamentLogManager::make(),
                 FilamentSpatieLaravelBackupPlugin::make()
                 ->usingPage(Backups::class),
+                // FilamentShieldPlugin::make()
                 FilamentShieldPlugin::make()
+                    ->gridColumns([
+                        'default' => 1,
+                        'sm' => 2,
+                        'lg' => 3
+                    ])
+                    ->sectionColumnSpan(1)
+                    ->checkboxListColumns([
+                        'default' => 1,
+                        'sm' => 2,
+                        'lg' => 4,
+                    ])
+                    ->resourceCheckboxListColumns([
+                        'default' => 1,
+                        'sm' => 2,
+                    ]),
+            ])
+            ->userMenuItems([
+                MenuItem::make()
+                    ->label('SD 1')
+                    ->url('https://pr-sd1.sekolahbasic.sch.id/')
+                    ->icon('heroicon-o-academic-cap'),
+                MenuItem::make()
+                    ->label('SD 2')
+                    ->url('https://pr-sd2.sekolahbasic.sch.id/')
+                    ->icon('heroicon-o-academic-cap'),
+                MenuItem::make()
+                    ->label('SMP 1')
+                    ->url('https://pr-smp1.sekolahbasic.sch.id/')
+                    ->icon('heroicon-o-academic-cap'),
+                MenuItem::make()
+                    ->label('SMP 2')
+                    ->url('https://pr-smp2.sekolahbasic.sch.id/')
+                    ->icon('heroicon-o-academic-cap'),
+                MenuItem::make()
+                    ->label('SMA 1')
+                    ->url('https://pr-sma1.sekolahbasic.sch.id/')
+                    ->icon('heroicon-o-academic-cap'),
+                MenuItem::make()
+                    ->label('SMA 2')
+                    ->url('https://pr-sma2.sekolahbasic.sch.id/')
+                    ->icon('heroicon-o-academic-cap'),
             ])
             ;
     }
